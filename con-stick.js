a02f461ca046a40f1080d0d8a5a3598daf8059c5
Christian Aldridge <Studiozut@gmail.com>
2016-05-19 11:38:00 -0400

    Cleanup and New Improved comments

--- con-stick.js
+++ con-stick.js
@@ -1,24 +1,20 @@
 <script>
 <!-- 
-  // Should add a class instead of styling the element. Also needs to recheck on window resize. Probably other things. 
+  // This checks how much space is below #footer. 
   var elem = document.getElementById('footer');
   var footspace = window.innerHeight - elem.offsetTop + elem.offsetHeight;
 
+  // "Wrap that sticky". Should add a class instead of styling the element. Also needs to recheck on window resize. Probably other things. 
   if(footspace > 10){
     elem.style.background = '#fff';
     elem.style.position = 'fixed';
     elem.style.bottom = '0';
   }
 
-// some alternate pokes at it 
-var d = document.getElementById('footer');
-var topPos = d.offsetTop;
+// Some alternate pokes at it. 
 
-if (topPos > 10) {
-  // object is offset more
-  // than 10 pixels from its parent
-  // d.style.position = 'fixed';
-  // d.style.bottom = '0';
-}
+// jquery.  
+// $('footer').offset().top;
+
 -->
 </script>
